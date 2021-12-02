80875c84c2b351ec544a370a1622ee4b015759c5
Sairoden <odingandarosa@gmail.com>
2021-12-02 14:20:56 +0800

    initial commit

---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Quote extends Component {
+  render() {
+    return (
+      <div className="Quote">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default Quote;
